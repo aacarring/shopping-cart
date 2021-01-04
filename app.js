44252ac3474bf9956edae1a136f5ed4f17e7126e
Alexa Carrington <acarring@wellesley.edu>
2021-01-04 12:17:46 -0500

    Edited total placeholder in html, edited styling, added selectors and clearCart, checkoutMsg functions in js

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 const addItemBtn = document.querySelectorAll('.content .drinks .beverage button');
+const cart = document.querySelector('.content .shopping-cart .cart-items');
+const total = document.querySelector('.content .shopping-cart .total p');
+const checkoutBtn = document.querySelector('.content .shopping-cart .checkout');
+let totalCount = 0;
+
+// also need to add button to each cart item to remove
 
 function addItem() {
     let item = this.parentElement;
@@ -6,9 +12,28 @@ function addItem() {
     let priceSpan = item.getElementsByClassName('price');
     let itemName = nameSpan[0].textContent;
     let itemPrice = parseFloat(priceSpan[0].textContent.replace('$', ''));
+    totalCount += itemPrice;
 
-   
+    cart.innerHTML += `
+    <div class="order">
+    <span class="order-item">${itemName}</span>
+    <span class="order-price">$ ${itemPrice}</span>
+    </div>
+    `
 
+    total.textContent = `Total: $${totalCount.toFixed(2)}`;
+}
+
+function clearCart() {
+    totalCount = 0;
+    total.textContent = "Total:";
+    cart.innerHTML = "";
+}
+
+function checkoutMsg() {
+    alert("We have recieved your order and will begin preparing your delivery. Thanks for choosing Blaue Blume Bevery! :)");
+    clearCart();
 }
 
 addItemBtn.forEach(btn => btn.addEventListener('click', addItem));
+checkoutBtn.addEventListener('click', checkoutMsg);
